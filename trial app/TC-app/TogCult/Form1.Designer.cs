88aa6764774b0394cdcd70de93ebbe507d6fc9cb
Dhoom0000 <dhoom.dev@outlook.com>
2024-10-14 17:15:07 +0100

    Add project files.

---
+++ trial app/TC-app/TogCult/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace TogCult
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.White;
+            ClientSize = new Size(782, 553);
+            Name = "Form1";
+            Padding = new Padding(10);
+            ShowIcon = false;
+            Text = "Together Culture";
+            TransparencyKey = Color.White;
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
